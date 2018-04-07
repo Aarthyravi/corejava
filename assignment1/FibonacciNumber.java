54e043a453f82ff0d28c449f821e528fa741f767
Aarthy Ravi <aarthydravi@gmail.com>
2018-04-07 10:33:53 -0700

    Add files via upload

---
+++ assignment1/FibonacciNumber.java
@@ -0,0 +1,35 @@
+package com.assignment1.util;
+
+import java.util.Scanner;
+
+public class FibonacciNumber {
+
+	public static void main(String[] args) {
+		int a=0,b=1,c=1;
+		System.out.println("\n\tFibonacci Series\n\t");
+		System.out.println("Enter the no of series : ");
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		System.out.println("\nOuput\n\t");
+		if(n == 1 || n == 0 ) {
+			System.out.println(a);
+		}
+		else if(n == 2) {
+			System.out.println(a);
+			System.out.println(b);
+	    }
+		else {
+			System.out.println(a);
+			System.out.println(b);
+			for(int i=2;i<n;i++) {
+				c = a + b;
+				a = b;
+				b = c;
+			    System.out.println(c);
+				
+		}
+	  }		
+		input.close();
+	}
+
+}
---
+++ assignment1/ThousandNumbers.java
@@ -0,0 +1,26 @@
+package com.assignment1.util;
+
+public class ThousandNumbers {
+
+	public static void main(String[] args) {
+		System.out.println("Generate 1000 Numbers");
+		for(int i=0;i<=1000;i++) {
+			System.out.print(i+",");
+		}
+		System.out.println("\n\nGenerate multiple of 5 and less than 1000");
+		for(int i=0;i<=1000;i++) {
+			if(i%5 == 0) {
+				int result = i;
+			    System.out.print(result+",");
+			}   
+		}
+		System.out.println("\n\nGenerate odd numbers up to 1000");
+		for(int i=0;i<=1000;i++) {
+			if(i%2 != 0) {
+				int result = i;
+			    System.out.print(result+",");
+			}   
+		}
+	}
+
+}
